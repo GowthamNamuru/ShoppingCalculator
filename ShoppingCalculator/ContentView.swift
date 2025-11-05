fd24cdb029e4a5dfe68c371c62218e4793f05f97
gowthamnamuru <gowthamnamuri@gmail.com>
2025-11-05 12:55:45 +0530

    Initial Commit

---
+++ ShoppingCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingCalculator
+//
+//  Created by Gowtham Namuru on 05/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoppingCalculator/ShoppingCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCalculatorApp.swift
+//  ShoppingCalculator
+//
+//  Created by Gowtham Namuru on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
