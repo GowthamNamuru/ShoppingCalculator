ef9c055524c2975c06fdb74ec4ce2aa052ee4835
gowthamnamuru <gowthamnamuri@gmail.com>
2025-11-05 13:50:35 +0530

    Added Final Discount calculator class

---
+++ ShoppingCalculator/OverallDiscountCalculator.swift
@@ -0,0 +1,36 @@
+//
+//  OverallDiscountCalculator.swift
+//  ShoppingCalculator
+//
+//  Created by Gowtham Namuru on 05/11/25.
+//
+
+import Foundation
+
+final class OverallDiscountCalculator {
+    private let discounts: [DiscountCalculator]
+    init(discounts: [DiscountCalculator]) {
+        self.discounts = discounts
+    }
+
+    func calculate(user: User, bill: Bill) {
+        var currentTotal = bill.subTotal
+        var percentageAppliedOn: String = ""
+        for discount in discounts {
+            let discountedValue = discount.apply(user: user, bill: bill, currentTotal: currentTotal)
+            if discount is PercentageDiscountCalculator {
+                percentageAppliedOn += "Percentage based Discounted value: \(discountedValue) \n"
+            } else {
+                percentageAppliedOn += "Amount based Discounted value: \(discountedValue) \n"
+            }
+            currentTotal = discountedValue
+        }
+    }
+}
+
+public extension Float {
+    func rounded(toPlaces places: Int) -> Float {
+        let divisor = pow(10.0, Float(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
